12586e65d5a50e63d6cdf1081f935ae3b03ec854
Rorazoro <rorazoro@gmail.com>
2021-01-30 12:33:01 -0500

    WIP Network Sync Movement

---
+++ Assets/Scripts/Managers/DebugCommandBase.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class DebugCommandBase {
+    private string _commandId;
+    private string _commandDescription;
+    private string _commandFormat;
+
+    public string commandId { get => _commandId; }
+    public string commandDescription { get => _commandDescription; }
+    public string commandFormat { get => _commandFormat; }
+
+    public DebugCommandBase (string id, string description, string format) {
+        _commandId = id;
+        _commandDescription = description;
+        _commandFormat = format;
+    }
+}
+
+public class DebugCommand : DebugCommandBase {
+    private Action command;
+
+    public DebugCommand (string id, string description, string format, Action command) : base (id, description, format) {
+        this.command = command;
+    }
+
+    public void Invoke () {
+        command.Invoke ();
+    }
+}
+
+public class DebugCommand<T> : DebugCommandBase {
+
+    private Action<T> command;
+
+    public DebugCommand (string id, string description, string format, Action<T> command) : base (id, description, format) {
+        this.command = command;
+    }
+
+    public void Invoke (T value) {
+        command.Invoke (value);
+    }
+}
